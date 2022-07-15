8c400ca67442135a0ad9664b5f9907511a36b3a1
asraful islam <asraful009@gmail.com>
2022-07-15 22:51:28 +0600

    testing download, delete

--- src/testing/app.spec.ts
+++ src/testing/app.spec.ts
@@ -6,6 +6,8 @@ import { rndomInt } from "../common/function/random-int.function";
 import configuration from "../common/config/configuration";
 import * as FormData from "form-data";
 import * as fs from "fs";
+import keyGenerator from "../common/function/key-generator.function";
+import { randomUUID } from "crypto";
 
 describe("App Testing", () => {
   let httpService: HttpService;
@@ -19,8 +21,9 @@ describe("App Testing", () => {
     httpService = await app.get<HttpService>(HttpService);
   });
 
-  describe("1> file api", () => {
-    it("should be upload files", async () => {
+  describe("file api", () => {
+    let onlineFiles: any[] = [];
+    it("upload", async () => {
       const filesPath = fs.readdirSync(process.env.TESTING_IMAGE_FOLDER);
       const form = new FormData();
       // console.log(filesPath);
@@ -41,7 +44,29 @@ describe("App Testing", () => {
       expect(payload["errorMsg"]).toBeNull();
     });
 
-    it("should be List", async () => {
+    it("List", async () => {
+      let limit = 10;
+      let page = 1;
+      let netCall = await firstValueFrom(
+        httpService.get(
+          `http://localhost:${process.env.PORT}/files/list?limit=${limit}&page=${page}`
+        )
+      );
+      let payload = netCall["data"];
+      // console.log(payload);
+      expect(payload["status"]).toBe(200);
+      expect(payload["errorMsg"]).toBeNull();
+      limit = -1;
+      page = 1;
+      netCall = await firstValueFrom(
+        httpService.get(
+          `http://localhost:${process.env.PORT}/files/list?limit=${limit}&page=${page}`
+        )
+      );
+      payload = netCall["data"];
+      onlineFiles = payload["payload"]["data"];
+      expect(payload["status"]).toBe(200);
+      expect(payload["errorMsg"]).toBeNull();
       for (let i = 0; i < 10; i++) {
         const limit = rndomInt(1, 20);
         const page = rndomInt(1, 20);
@@ -54,6 +79,68 @@ describe("App Testing", () => {
         // console.log(payload);
         expect(payload["status"]).toBe(200);
         expect(payload["errorMsg"]).toBeNull();
+      }
+    });
+
+    it("download", async () => {
+      for (let i = 0; i < 10; i++) {
+        const token = keyGenerator({ id: randomUUID(), file: {} });
+        try {
+          let netCall = await firstValueFrom(
+            httpService.get(
+              `http://localhost:${process.env.PORT}/files/${token.publicToken}`
+            )
+          );
+        } catch (error) {
+          // const payload = error["data"];
+          // console.log(payload);
+          expect(error["response"]["status"]).toBe(403);
+          // expect(payload["errorMsg"]).not.toBeNull();
+          // console.log(error);
+        }
+      }
+      for (const onlineFile of onlineFiles) {
+        try {
+          let netCall = await firstValueFrom(
+            httpService.get(
+              `http://localhost:${process.env.PORT}/files/${onlineFile["publicToken"]}`
+            )
+          );
+          expect(netCall["status"]).toBe(200);
+        } catch (error) {
+          expect(error["response"]["status"]).toBe(403);
+        }
+      }
+    });
+
+    it("delete", async () => {
+      for (let i = 0; i < 10; i++) {
+        const token = keyGenerator({ id: randomUUID(), file: {} });
+        try {
+          let netCall = await firstValueFrom(
+            httpService.delete(
+              `http://localhost:${process.env.PORT}/files/${token.privateToken}`
+            )
+          );
+        } catch (error) {
+          // const payload = error["data"];
+          // console.log(payload);
+          expect(error["response"]["status"]).toBe(403);
+          // expect(payload["errorMsg"]).not.toBeNull();
+          // console.log(error);
+        }
+      }
+      for (const onlineFile of onlineFiles) {
+        try {
+          let netCall = await firstValueFrom(
+            httpService.delete(
+              `http://localhost:${process.env.PORT}/files/${onlineFile["privateToken"]}`
+            )
+          );
+          expect(netCall["status"]).toBe(200);
+        } catch (error) {
+          expect(error["response"]["status"]).toBe(403);
+        }
       }
     });
   });
