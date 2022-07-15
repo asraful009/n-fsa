203dd6492571e815499f955a090b7eccee719ee3
asraful islam <asraful009@gmail.com>
2022-07-15 21:44:09 +0600

    testing upload , list

--- src/testing/app.spec.ts
+++ src/testing/app.spec.ts
@@ -4,6 +4,8 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { firstValueFrom } from "rxjs";
 import { rndomInt } from "../common/function/random-int.function";
 import configuration from "../common/config/configuration";
+import * as FormData from "form-data";
+import * as fs from "fs";
 
 describe("App Testing", () => {
   let httpService: HttpService;
@@ -17,14 +19,35 @@ describe("App Testing", () => {
     httpService = await app.get<HttpService>(HttpService);
   });
 
-  describe("1> file List", () => {
-    it('should return "tokens"', async () => {
+  describe("1> file api", () => {
+    it("should be upload files", async () => {
+      const filesPath = fs.readdirSync(process.env.TESTING_IMAGE_FOLDER);
+      const form = new FormData();
+      // console.log(filesPath);
+      for (const filePath of filesPath) {
+        form.append(
+          "files",
+          fs.createReadStream(`${process.env.TESTING_IMAGE_FOLDER}/${filePath}`)
+        );
+      }
+      let netCall = await firstValueFrom(
+        httpService.post(`http://localhost:${process.env.PORT}/files`, form, {
+          headers: form.getHeaders(),
+        })
+      );
+      const payload = netCall["data"];
+      //console.log(payload);
+      expect(payload["status"]).toBe(201);
+      expect(payload["errorMsg"]).toBeNull();
+    });
+
+    it("should be List", async () => {
       for (let i = 0; i < 10; i++) {
         const limit = rndomInt(1, 20);
         const page = rndomInt(1, 20);
         let netCall = await firstValueFrom(
           httpService.get(
-            `http://localhost:8080/files/list?limit=${limit}&page=${page}`
+            `http://localhost:${process.env.PORT}/files/list?limit=${limit}&page=${page}`
           )
         );
         const payload = netCall["data"];
